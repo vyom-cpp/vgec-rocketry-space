a15d0f164ca7358fadda146cd43ed2dafefaf89e
vyom-cpp <vyom.sutariya0948@gmail.com>
2024-08-12 23:46:25 +0530

    Support us page framework is almost ready

--- apps/frontend/src/app/pages/SupportUs.tsx
+++ apps/frontend/src/app/pages/SupportUs.tsx
@@ -1,25 +1,236 @@
-import { Box, Container, ListItem, Stack } from "@mui/material"
-import React, { useEffect } from "react" 
-import { Navbar } from "../components/Navbar"
+import { Box, Container, Button, Stack, Typography, useMediaQuery } from "@mui/material";
+import React, { useEffect } from "react";
+// import { motion } from "framer-motion";
+import { Navbar } from "../components/Navbar";
+import { theme } from "../theme";
+import { Footer } from "../components/Footer";
+import { useInView } from "react-intersection-observer";
+
 export const SupportUs: React.FC = () => {
 
     useEffect(() => {
-        document.title = "SUPPORT US | VRT"
-        document
-      }, [])
-    
-    return (
-        <Container maxWidth={false} 
-            sx={{
-                backgroundImage: "url('../../public/image3.jpeg')",
-                backgroundColor: "black",
-                height: "100vh"
-            }}
-        >
-                <Stack>
-                        <Navbar/>  
-                </Stack>
-        </Container>
-    )
+        document.title = "SUPPORT US | VRT";
+    }, []);
 
-}
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+    // const { ref: storyRef, inView: storyInView } = useInView({
+    //     triggerOnce: true,
+    //     threshold: 0.1
+    // });
+
+    // const fadeInVariants = {
+    //     hidden: { opacity: 0, y: 20 },
+    //     visible: { opacity: 1, y: 0, transition: { duration: 0.8 } }
+    // };
+
+    return (
+        <div style={{ overflow: "hidden" }}>
+            <Stack
+                sx={{
+                    backgroundColor: "black",
+                    margin: 0,
+                    padding: 0,
+                    border: "none"
+                }}
+            >
+                <Navbar />
+            </Stack>
+
+            <Container
+                maxWidth={false}
+                sx={{
+                    backgroundColor: "black",
+                    width: "100%",
+                    height: "500px",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    marginBottom: "0px",
+                    padding: 0,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    border: "none"
+                }}
+            >
+                <Typography
+                    sx={{
+                        color: "white",
+                        fontFamily: theme.typography.fontFamily,
+                        fontSize: isSmallScreen ? "70px" : "90px"
+                    }}
+                >
+                    Invest in the future.
+                </Typography>
+
+                <Typography
+                    sx={{
+                        color: "white",
+                        fontFamily: theme.typography.fontFamily,
+                        fontSize: isSmallScreen ? "45px" : "60px"
+                    }}
+                >
+                    Contribute today to empower the next generation.
+                </Typography>
+            </Container>
+
+            {/* Quote */}
+            <Container
+                maxWidth={false}
+                sx={{
+                    backgroundColor: "white",
+                    width: "100%",
+                    height: "200px",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    marginBottom: "0px",
+                    padding: 0,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    border: "none"
+                }}
+            >
+                <Typography
+                    sx={{
+                        color: "black",
+                        fontFamily: theme.typography.fontFamily,
+                        fontSize: isSmallScreen ? "30px" : "50px"
+                    }}
+                >
+                    “Rockets are cool. There's no getting around them”
+                    <br />
+                    ~Elon Musk
+                </Typography>
+            </Container>
+
+            {/* Tab line */}
+            <Container
+                maxWidth={false}
+                sx={{
+                    marginTop: "64px",
+                    marginBottom: "64px",
+                    boxSizing: "border-box",
+                    minWidth: 0,
+                    position: "relative",
+                    width: "100%",
+                    textAlign: "center",
+                    "&::before": {
+                        content: '""',
+                        position: "absolute",
+                        left: "10%",
+                        right: "10%",
+                        bottom: 0,
+                        borderBottom: "1px solid #000",
+                        width: "80%",
+                        margin: "0 auto"
+                    },
+                }}
+            />
+
+            <Container
+                maxWidth={false}
+                sx={{
+                    marginTop: isSmallScreen ? "100px" : "230px",
+                    marginBottom: "64px",
+                    padding: 0,
+                    textAlign: "center",
+                    width: "100%"
+                }}
+            >
+                <Typography
+                    sx={{
+                        color: "black",
+                        fontFamily: theme.typography.fontFamily,
+                        fontSize: isSmallScreen ? "50px" : "80px",
+                        marginBottom: "32px"
+                    }}
+                >
+                    Our Rockets
+                </Typography>
+
+                <Stack
+                    direction="row"
+                    spacing={isSmallScreen ? 2 : 4}
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    <Box sx={{ textAlign: "center" }}>
+                        <img
+                            src="/path/to/image1.jpg"
+                            alt="Rocket One"
+                            style={{ width: isSmallScreen ? "80%" : "300px", height: "auto" }}
+                        />
+                        <Typography
+                            sx={{
+                                color: "black",
+                                fontFamily: theme.typography.fontFamily,
+                                fontSize: isSmallScreen ? "18px" : "24px",
+                                marginTop: "8px"
+                            }}
+                        >
+                            Rocket One
+                            <br />
+                            Height: 180m
+                        </Typography>
+                    </Box>
+
+                    <Box sx={{ textAlign: "center" }}>
+                        <img
+                            src="/path/to/image2.jpg"
+                            alt="Rocket Two"
+                            style={{ width: isSmallScreen ? "80%" : "300px", height: "auto" }}
+                        />
+                        <Typography
+                            sx={{
+                                color: "black",
+                                fontFamily: theme.typography.fontFamily,
+                                fontSize: isSmallScreen ? "18px" : "24px",
+                                marginTop: "8px"
+                            }}
+                        >
+                            Rocket Two
+                            <br />
+                            Height: 240m
+                        </Typography>
+                    </Box>
+                </Stack>
+            </Container>
+
+            {/* Sponsor */}
+            <Container
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    marginTop: "16px",
+                    marginBottom: "64px"
+                }}
+            >
+                <Button
+                    variant="outlined"
+                    sx={{
+                        width: isSmallScreen ? "80%" : "80%",
+                        maxWidth: "500px",
+                        padding: "10px 20px",
+                        borderColor: 'black',
+                        color: 'black',
+                        fontSize: isSmallScreen ? '14px' : '18px', // Custom font size
+                        '&:hover': {
+                            backgroundColor: 'black',
+                            color: 'white',
+                        },
+                    }}
+                    href="mailto:vrt@gmail.com?subject=Sponsoring"
+                >
+                    Donate Us
+                </Button>
+            </Container>
+
+            <Footer isSmallScreen={isSmallScreen} />
+        </div>
+    );
+};
